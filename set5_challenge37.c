000cb99ac2a2ca687844f870f5bd9979a7364aa8
David Koenig <davidkoenig@noreply>
2020-08-11 08:45:24 -0700

    Update usage statement

--- set5_challenge37.c
+++ set5_challenge37.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char ** argv) {
     if (argc < 4) {
-        fprintf(stderr, "Usage: %s seed email password\nSecure Remote Password (SRP)\n", argv[0]);
+        fprintf(stderr, "Usage: %s seed email password\nHack Secure Remote Password (SRP)\n", argv[0]);
         return 1;
     }
     unsigned int seed = atoi(argv[1]);
