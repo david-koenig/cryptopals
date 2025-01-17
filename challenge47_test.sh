3171c5564fbe1f45564e55cef899cccc41524795
David Koenig <davidkoenig@noreply>
2025-01-17 10:38:45 -0800

    Fix to challenge scripts to work properly on Ubuntu and Mac

--- challenge47_test.sh
+++ challenge47_test.sh
@@ -2,5 +2,5 @@
 set -ex
 
 for i in $(seq 1 20); do
-    ./set6_challenge47 $RANDOM
+    ./set6_challenge47 "$RANDOM"
 done
--- challenge48_test.sh
+++ challenge48_test.sh
@@ -3,5 +3,5 @@ set -ex
 
 for i in $(seq 1 20); do
     ARG=$(cat /dev/random | LC_ALL=C tr -dc '[:alnum:]' | head -c 80)
-    ./set6_challenge48 $RANDOM "$ARG"
+    ./set6_challenge48 "$RANDOM" "$ARG"
 done
